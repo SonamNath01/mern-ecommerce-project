8a30e3cea9615e9669b0fef015e51901292aee0f
SonamNath01 <nathsonam2005@gmail.com>
2025-09-21 02:38:46 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Card from './pages/Card'
+import Collection from './pages/Collection'
+import Products from './pages/Products'   
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/card' element={<Card />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/product/:productId' element={<Products />} />
+        <Route path='/cart' element={<Card />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/order' element={<Orders />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
